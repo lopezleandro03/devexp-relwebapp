c67ea96f0cbea1d60d5915b565fba0415705412c
Remco Brosky <remcobrosky@microsoft.com>
2023-04-17 12:36:02 +0200

    added some bad code

---
+++ src/Relecloud.Web/BadCode.cs
@@ -0,0 +1,37 @@
+﻿using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace Relecloud.Web;
+
+public class BadCode
+{
+    //XSS vulnerability
+    public async Task XssProcessRequest(HttpContext ctx)
+    {
+        await ctx.Response.WriteAsync(
+            "The page \"" + ctx.Request.Query["page"] + "\" was not found.");
+    }
+
+    //SQL injection vulnerability
+    public async Task SqlInjectionProcessRequest(HttpContext ctx)
+    {
+        var page = ctx.Request.Query["page"];
+        var sql = "SELECT * FROM pages WHERE name = '" + page + "'";
+        var cmd = new SqlCommand(sql, new SqlConnection("..."));
+        var reader = await cmd.ExecuteReaderAsync();
+        
+    }
+
+    //Regex DDOS
+    public void RegExProcessRequest(HttpContext ctx)
+    {
+        string userInput = ctx.Request.Query["userInput"];
+
+        // BAD: User input is matched against a regex with exponential worst case behavior
+        new Regex("^([a-z]*)*$").Match(userInput);
+
+        
+    }
+
+    
+}
